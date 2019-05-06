a46f311faf7dcc77018172a402e8f048ccfa3601
Eternity Duraz <eternity103193@hotmail.com>
2019-05-06 03:49:57 -0400

    Add project files.

---
+++ GraphSearch/Algorithm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearch
+{
+    public class Algorithms
+    {
+
+        public Func<T, IEnumerable<T>> ShortestPathFunction<T>(Graph<T> graph, T start)
+        {
+            var previous = new Dictionary<T, T>();
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                {
+                    if (previous.ContainsKey(neighbor))
+                        continue;
+
+                    previous[neighbor] = vertex;
+                    queue.Enqueue(neighbor);
+                }
+            }
+
+            Func<T, IEnumerable<T>> shortestPath = v => {
+                var path = new List<T> { };
+
+                var current = v;
+                while (!current.Equals(start))
+                {
+                    path.Add(current);
+                    current = previous[current];
+                };
+
+                path.Add(start);
+                path.Reverse();
+
+                return path;
+            };
+
+            return shortestPath;
+        }
+        public HashSet<T> BFS<T>(Graph<int> graph1, Graph<T> graph, T start)
+        {
+            var visited = new HashSet<T>();
+
+            if (!graph.AdjacencyList.ContainsKey(start))
+                return visited;
+
+            var queue = new Queue<T>();
+            queue.Enqueue(start);
+
+            while (queue.Count > 0)
+            {
+                var vertex = queue.Dequeue();
+
+                if (visited.Contains(vertex))
+                    continue;
+
+                visited.Add(vertex);
+
+                foreach (var neighbor in graph.AdjacencyList[vertex])
+                    if (!visited.Contains(neighbor))
+                        queue.Enqueue(neighbor);
+            }
+
+            return visited;
+        }
+    }
+}
+
---
+++ GraphSearch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphSearch
+{
+   
+        public class Program
+        {
+            public static void Main(string[] args)
+            {
+            var vertices = new[] { 1, 12, 19, 21, 7, 31, 14};
+            var edges = new[]{
+                            Tuple.Create(1,12), Tuple.Create(1,21), Tuple.Create(1,7),
+                            Tuple.Create(12,19),
+                            Tuple.Create(7,21),
+                            Tuple.Create(19,1), Tuple.Create(19,21),
+                            Tuple.Create(21,14), Tuple.Create(21,31),
+                            Tuple.Create(14,14)};
+
+            var graph = new Graph<int>(vertices, edges);
+                var algorithms = new Algorithms();
+
+            var startVertex = 1;
+                var shortestPath = algorithms.ShortestPathFunction(graph, startVertex);
+                foreach (var vertex in vertices)
+                    Console.WriteLine("Shortest Path to {0,2}: {1}",
+                            vertex, string.Join(", ", shortestPath(vertex)));
+
+            Console.ReadKey();
+            }
+        }
+    }
+    
